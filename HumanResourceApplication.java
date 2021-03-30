d74eed9985bc171594c79b40d37332fe9f161efe
Kato Damian <katodakam2010@gmail.com>
2021-03-30 01:25:39 -0600

    Resolved some issue in main method

--- HumanResourceApplication.java
+++ HumanResourceApplication.java
@@ -13,12 +13,7 @@ public class HumanResourceApplication {
 		LocalDate odate1= LocalDate.of(2021, 10, 6);
 		LocalDate odate2= LocalDate.of(2021, 02, 7);
 		LocalDate odate3= LocalDate.of(2021, 03, 7);
-		
-	 
-		Period age = Period.between(odate1, odate3);  
-		LocalDate ldt =odate1.plusMonths(3);
-		System.out.println(ldt);
-					
+							
 		Order myorder = new Order("O15526", odate1, 600000);
 		Order myorder2 = new Order("O9299", odate2, 200000);
 		Order myorder3 = new Order("O82773", odate3, 15000);
